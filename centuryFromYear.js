baa00e979cfb25fd5d9054dcb108a93befcc8983
Red6eard <vadim.71rus@gmail.com>
2025-03-05 16:37:40 +0300

    решение задачи про века

--- centuryFromYear.js
+++ centuryFromYear.js
@@ -12,7 +12,21 @@
 2742 --> 28
 */
 function century(year) {
-    // Finish this :)
-    return;
+    let century = year / 100;
+    if (Number.isInteger(century)) {
+        return century;
+    } else {
+        return century = Math.floor(century) + 1;
+    };
 };
+console.log(century(2025));
 
+/*
+Number.isInteger() - метод проверяет является ли число целым.
+Math.floor() - метод возвращает целое число округленное вниз.
+Math.ceil() - метод возвращает целое число округленное вверх.
+*/
+
+// еще вариант решения:
+let century2 = (year) => Math.ceil(year / 100);
+console.log(century2(2025));
