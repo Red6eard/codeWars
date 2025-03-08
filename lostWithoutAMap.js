f49a48dca9e328bb0050351eb02192e3582f1043
Red6eard <vadim.71rus@gmail.com>
2025-03-08 07:22:52 +0300

    update

--- lostWithoutAMap.js
+++ lostWithoutAMap.js
@@ -2,8 +2,13 @@
 Дан массив целых чисел, вернуть новый массив, в котором каждое значение удвоено.
 Например:
 [1, 2, 3] --> [2, 4, 6]
-*/
+
 
 function maps(x){
+  let newArr = x.map((el) => el * 2);
+  return newArr;
+};
+*/
+let maps = (x) => x.map((el) => el * 2);
 
-}
+console.log(maps([2,3,5]))
