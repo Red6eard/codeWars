87a4a9c1091cccc4b877a4a700252966e315666a
Red6eard <vadim.71rus@gmail.com>
2025-03-09 16:12:03 +0300

    update countOddNumbersBelowN.js

---
+++ countOddNumbersBelowN.js
@@ -0,0 +1,11 @@
+/*
+Дано число n, верните количество положительных нечетных чисел ниже n. ЛЕГКО!
+
+Примеры (Вход -> Выход)
+
+7 - > 3(because odd numbers below 7 are[1, 3, 5])
+15 - > 7(because odd numbers below 15 are[1, 3, 5, 7, 9, 11, 13])
+*/
+function oddCount(n) {
+  // your code here
+}
