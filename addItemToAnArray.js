8ff6e22ac6f231ea7be52bd587668c04e72384ff
Red6eard <vadim.71rus@gmail.com>
2025-03-16 10:54:07 +0300

    update

---
+++ addItemToAnArray.js
@@ -0,0 +1,14 @@
+/*
+Добавьте значение "codewars"
+в массив websites.
+После выполнения вашего кода массив websites должен == ["codewars"]
+
+Массив веб - сайтов уже определен для вас с помощью следующего кода:
+  
+  var websites = [];
+*/
+
+let websites = [];
+websites.push('codewars');
+
+console.log(websites);
