92478a8a04a82dcdb7f876c35900ee77ebddfbbb
Red6eard <vadim.71rus@gmail.com>
2025-03-21 18:24:02 +0300

    task complite

--- squareNSum.js
+++ squareNSum.js
@@ -5,6 +5,10 @@
 1^2+2^2+2^2=9.
 */
 function squareSum(numbers){
-
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+         sum += numbers[i]**2;
+    };
+    return sum;
 }
-console.log();
+console.log(squareSum([1,2,3]));
