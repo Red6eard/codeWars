932fda92ccedc212d77f286b7e049eb8aa3ddcb6
Red6eard <vadim.71rus@gmail.com>
2025-03-23 23:13:13 +0300

    update

---
+++ findTheRemainder.js
@@ -0,0 +1,22 @@
+/*
+Напишите функцию, которая принимает два целых числа и возвращает остаток от деления большего значения на меньшее значение.
+Деление на ноль должно вернуть NaN.
+Примеры:
+  n = 17
+m = 5
+result = 2(remainder of `17 / 5`)
+
+n = 13
+m = 72
+result = 7(remainder of `72 / 13`)
+
+n = 0
+m = -1
+result = 0(remainder of `0 / -1`)
+*/
+function remainder(n, m) {
+  // Divide the larger argument by the smaller argument and return the remainder
+  
+};
+
+console.log()
