9a71fe8a4444bb85b1635af5e15b5c3d94ca1e17
Red6eard <vadim.71rus@gmail.com>
2025-04-14 19:10:44 +0300

    update

--- grader.js
+++ grader.js
@@ -1,11 +1,11 @@
 /*
 Создайте функцию, которая принимает число в качестве аргумента и возвращает оценку на основе этого числа.
 Счет Оценка
-Все, что больше 1 или меньше 0, 6 "Ф"
-0, 9 или больше "А"
-0, 8 или больше "Б"
-0, 7 или больше "С"
-0, 6 или больше "Д"
+Все, что больше 1 или меньше 0,6 "Ф"
+0,9 или больше "А"
+0,8 или больше "Б"
+0,7 или больше "С"
+0,6 или больше "Д"
 Примеры:
 grader(0) should be "F"
 grader(1.1) should be "F"
@@ -15,7 +15,21 @@ grader(0.7) should be "C"
 grader(0.6) should be "D"
 */
 function grader(score) {
-  
+  switch (true) {
+    case (score < 0.6 || score > 1):
+      return "F";
+    case (score >= 0.9):
+      return "A";
+    case (score >= 0.8):
+      return "B";
+    case (score >= 0.7):
+      return "C";
+    case (score >= 0.6):
+      return "D";
+    default:
+      return "F"; 
+  }
 };
 
-console.log();
+// Примеры использования
+console.log(grader(0.99));
