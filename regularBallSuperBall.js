2fe57a829d96c529c8409f8bd56bbfb85599d88a
Red6eard <vadim.71rus@gmail.com>
2025-04-19 13:50:34 +0300

    update

---
+++ regularBallSuperBall.js
@@ -0,0 +1,18 @@
+/*
+Создайте класс Ball. Объекты Ball должны принимать один аргумент для «типа мяча» при создании экземпляра.
+
+Если аргументы не указаны, объекты мяча должны создаваться с «типом мяча» «обычный».
+
+ball1 = new Ball();
+ball2 = new Ball("super");
+
+ball1.ballType     //=> "regular"
+ball2.ballType     //=> "super"
+
+*/
+
+var Ball = function(ballType) {
+  // your code goes here
+};
+
+console.log()
